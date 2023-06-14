5353356ecd0fc72caa896cabe6f5fb0a5fee7245
leaba <lea.barlet@ecam.fr>
2023-06-14 19:33:40 -0300

    convrs

--- Ejercicios-JavaScript/misFunciones.js
+++ Ejercicios-JavaScript/misFunciones.js
@@ -5,3 +5,30 @@
  * @param Parámetro B
  * @return Valor que retorna
  */
+
+function cambiarUnidades(id, valor){
+    if (isNaN(valor)) {
+        alert("valor invalido");
+        document.lasUnidades.unid_metro.value = "";
+        document.lasUnidades.unid_yarda.value = "";
+        document.lasUnidades.unid_pulgada.value = "";
+        document.lasUnidades.unid_pie.value = "";
+    } else if (id == "metro") {
+        document.lasUnidades.unid_pulgada.value = valor * 39.3701;
+        document.lasUnidades.unid_pie.value = valor * 3.28084;
+        document.lasUnidades.unid_yarda.value = valor * 1.09361;
+    } else if (id == "pulgada") {
+        document.lasUnidades.unid_metro.value = valor * 0.0254;
+        document.lasUnidades.unid_pie.value = valor * 0.08333;
+        document.lasUnidades.unid_yarda.value = valor * 0.027778;
+    } else if (id == "pie") {
+        document.lasUnidades.unid_metro.value = valor * 0.3048;
+        document.lasUnidades.unid_pulgada.value = valor * 12;
+        document.lasUnidades.unid_yarda.value = valor * 0.333333;
+    } else if (id == "yarda") {
+        document.lasUnidades.unid_metro.value = valor * 0.9144;
+        document.lasUnidades.unid_pulgada.value = valor * 36;
+        document.lasUnidades.unid_pie.value = valor * 3;
+    }
+
+}
