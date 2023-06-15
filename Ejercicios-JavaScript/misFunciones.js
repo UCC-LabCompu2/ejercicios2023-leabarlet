259e36344686f8d8387dbe8ef0789ace2cdcc55a
leaba <lea.barlet@ecam.fr>
2023-06-14 21:48:40 -0300

    done

--- Ejercicios-JavaScript/misFunciones.js
+++ Ejercicios-JavaScript/misFunciones.js
@@ -21,24 +21,24 @@ function cambiarUnidades(id,valor) {
         yar = "";
         alert("El valor ingresado es incorrecto");
     }
-    else if(id=="metro"){
+    else if(id==="metro"){
         // Conversion metro a unidades
         met = valor;
         yar = 1.09361*valor;
         pie = 3.28084*valor;
         pul = 39.3701*valor;
     }
-    else if (id="yarda"){
+    else if (id === "yarda"){
         pul= 36*valor;
         pie = 3*valor;
         met = 0.9144*valor;
     }
-    else if (id="pie"){
+    else if (id === "pie"){
         pul = 12*valor;
         met = 0.3048*valor;
         yar = 0.333333*valor;
     }
-    else if (id="pulgada") {
+    else if (id === "pulgada") {
         met = 0.0254 * valor;
         yar = 0.0277778 * valor;
         pie = 0.0833333 * valor;
@@ -51,29 +51,29 @@ function cambiarUnidades(id,valor) {
 }
 
 /**
- * Permite convertir unidades gradios a radianes y viceversa
- * @method Nombre de la función
+ * Permite convertir unidades gradios a radianes y vice-versa
+ * @method convertirGR
  * @param {string} id -Id del input del formulario
  * @return
  */
-let convertirGR=(id)=>{
+let convertirGR;
+convertirGR = (id) => {
     let gr, rad;
-    if(id=="grados"){
+    if (id === "grados") {
         gr = document.getElementById("grados").value;
-        rad = (gr*Math.PI)/180;
-    }
-    else{
+        rad = (gr * Math.PI) / 180;
+    } else {
         rad = document.getElementById("radianes").value;
-        gr = (rad*180)/Math.PI;
+        gr = (rad * 180) / Math.PI;
 
     }
     // Asignacion Final
     document.getElementById("radianes").value = rad;
     document.getElementById("grados").value = gr;
-}
+};
 
 /**
- * Permite visualizar y ocultar un dov centro de la pagina
+ * Permite visualizar y ocultar un div dentro de la pagina
  * @method mostrar_ocultrar
  * @param {string} valor - Valor asociado a un radio button del html
  * @return Valor que retorna
@@ -90,11 +90,11 @@ function mostrar_ocultar(valor){
 /**
  * Permite calcular la suma
  * @method sumar
- * @param
+ * @para
  * @return
  */
 function sumar() {
-    var num1, num2;
+    let num1, num2;
     num1 = document.getElementById("nums1").value;
     num2 = document.getElementById("nums2").value;
 
@@ -107,7 +107,7 @@ function sumar() {
  * @return
  */
 function sous() {
-    var num1, num2;
+    let num1, num2;
     num1 = document.getElementById("numr1").value;
     num2 = document.getElementById("numr2").value;
 
@@ -120,7 +120,7 @@ function sous() {
  * @return
  */
 function mult() {
-    var num1, num2;
+    let num1, num2;
     num1 = document.getElementById("numm1").value;
     num2 = document.getElementById("numm2").value;
 
@@ -133,7 +133,7 @@ function mult() {
  * @return
  */
 function div() {
-    var num1, num2;
+    let num1, num2;
     num1 = document.getElementById("numd1").value;
     num2 = document.getElementById("numd2").value;
 
@@ -147,7 +147,7 @@ function div() {
  * @return
  */
 function cargarWeb(){
-    var cant, unidad, urlComp;
+    let cant, unidad, urlComp;
 
     cant = document.getElementById("distancia").value;
     unidad = document.getElementsByName("unidades")[0].value;
@@ -164,7 +164,7 @@ function cargarWeb(){
  * @return
  */
 function cargarResultado(){
-    var urlComp, can, uni;
+    let urlComp, can, uni;
 
     urlComp = window.location.href.split("/")[5];
     can = urlComp.split("#")[1];
@@ -207,7 +207,12 @@ function cargarLocalStorage(){
     document.getElementById("dist").value = cant + " "+ un;
 
 }
-
+/**
+ * Permite dibujar un circulo y un cuardrado en el canvas
+ * @method dibujarCirCuad
+ * @param
+ * @return
+ */
 function dibujarCirCuad(){
     var canvas = document.getElementById("myCanvas");
     var ctx = canvas.getContext("2d");
@@ -216,7 +221,7 @@ function dibujarCirCuad(){
     var yMax = canvas.height;
     var margen = 5;
     ctx.fillStyle = "#333093";
-    ctx.fillRect(0+margen,yMax-40 - margen,40,40);
+    ctx.fillRect(margen,yMax-40 - margen,40,40);
 
     ctx.arc(xMax/2, yMax/2,20, 0,2*Math.PI);
     ctx.stroke();
@@ -225,7 +230,14 @@ function dibujarCirCuad(){
 
 }
 
-var bandera;
+/**
+ * Permite dibujar con el mouse
+ * @method dibujar
+ * @param event
+ * @return
+ */
+let bandera;
+
 function dibujar(event) {
     var canvas = document.getElementById("canvasAdibujar");
     var ctx = canvas.getContext("2d");
@@ -241,7 +253,12 @@ function dibujar(event) {
     }
 
 }
-
+/**
+ * Permite limpiar el canvas
+ * @method limpiarCanvas
+ * @param
+ * @return
+ */
 function limpiarCanvas() {
     var canvas = document.getElementById("canvasAdibujar");
     var ctx = canvas.getContext("2d");
@@ -249,7 +266,12 @@ function limpiarCanvas() {
     canvas.width = canvas.width;
 
 }
-
+/**
+ * Permite dibujar un cuadriculado
+ * @method dibujarCuadriculado
+ * @param
+ * @return
+ */
 function dibujarCuadriculado() {
     var canvas = document.getElementById("myCanvas");
     var ctx = canvas.getContext("2d");
@@ -291,7 +313,13 @@ function dibujarCuadriculado() {
     ctx.stroke();
     ctx.closePath();
 }
-
+/**
+ * Permite mostrar una imagen en un canvas
+ * @method dibujarImagen
+ * @param posX
+ * @param posY
+ * @return
+ */
 function dibujarImagen(posX,posY){
     var canvas = document.getElementById("myCanvas");
     var ctx = canvas.getContext("2d");
@@ -307,6 +335,12 @@ function dibujarImagen(posX,posY){
 
 }
 
+/**
+ * Permite animar el movimiento de la imagen
+ * @method animarAuto
+ * @param
+ * @return
+ */
 x=0;
 dx=2;
 function animarAuto(){
@@ -324,5 +358,4 @@ function animarAuto(){
         x=0;
     }
     x+=dx;
-
 }
